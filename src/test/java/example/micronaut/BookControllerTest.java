b8a3ba002958abdc696bf0d6210c3c7dbcd30eea
leadtrip <woodm@objectcomputing.com>
2023-04-11 15:19:16 +0100

    Test updated to use the same genres every time

--- src/test/java/example/micronaut/BookControllerTest.java
+++ src/test/java/example/micronaut/BookControllerTest.java
@@ -21,8 +21,7 @@ import org.junit.jupiter.api.TestInstance;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -37,11 +36,15 @@ public class BookControllerTest {
 
     @Test
     public void testBookCrudOperations() {
+        String progGenre = "Programming";
+        String dbGenre = "Database";
+        String thrGenre = "Threads";
+
         // add some genres
         Set<Genre> genres = new HashSet<>();
 
         // genre 1
-        HttpRequest<?> request = HttpRequest.POST("/genres", Collections.singletonMap("name", "Programming"));
+        HttpRequest<?> request = HttpRequest.POST("/genres", Collections.singletonMap("name", progGenre));
         HttpResponse<?> response = httpClient.toBlocking().exchange(request);
         assertEquals(HttpStatus.CREATED, response.getStatus());
         UUID genreId = entityId(response, "/genres/");
@@ -51,7 +54,7 @@ public class BookControllerTest {
         // =============================
 
         // genre 2
-        request = HttpRequest.POST("/genres", Collections.singletonMap("name", "Database"));
+        request = HttpRequest.POST("/genres", Collections.singletonMap("name", dbGenre));
         response = httpClient.toBlocking().exchange(request);
         assertEquals(HttpStatus.CREATED, response.getStatus());
         genreId = entityId(response, "/genres/");
@@ -61,7 +64,7 @@ public class BookControllerTest {
         // =============================
 
         // genre 3
-        request = HttpRequest.POST("/genres", Collections.singletonMap("name", "JSON"));
+        request = HttpRequest.POST("/genres", Collections.singletonMap("name", thrGenre));
         response = httpClient.toBlocking().exchange(request);
         assertEquals(HttpStatus.CREATED, response.getStatus());
         genreId = entityId(response, "/genres/");
@@ -73,10 +76,14 @@ public class BookControllerTest {
         Long totalGenres = (long) genres.size();
         assertEquals(3, totalGenres);
 
-        // add a book with 2 genres
+        Genre programming = genres.stream().filter(g-> Objects.equals(g.getName(), progGenre)).findFirst().get();
+        Genre database = genres.stream().filter(g-> Objects.equals(g.getName(), dbGenre)).findFirst().get();
+        Genre threads = genres.stream().filter(g-> Objects.equals(g.getName(), thrGenre)).findFirst().get();
+
+        // add a book with 2 genres, programming and database
         BookCreateCommand bookCreateCommand = new BookCreateCommand(
                 "Head first Java",
-                genres.stream().map(Genre::getId).limit(2).collect(Collectors.toSet()));
+                Set.of(programming, database).stream().map(Genre::getId).collect(Collectors.toSet()));
 
         request = HttpRequest.POST("/books", bookCreateCommand);
         response = httpClient.toBlocking().exchange(request);
@@ -87,16 +94,18 @@ public class BookControllerTest {
         UUID bookId = entityId(response, "/books/");
         request = HttpRequest.GET("/books/" + bookId);
         Book fetchedBook = httpClient.toBlocking().retrieve(request, Book.class);
-        System.out.println(fetchedBook);
+
         assertEquals( totalGenres , genreRepository.count().block() );
         assertEquals( totalGenres - 1, fetchedBook.getGenres().size());
+        assertNotNull(findGenre(progGenre, fetchedBook), "Programming genre is not present");
+        assertNotNull(findGenre(dbGenre, fetchedBook), "Database genre is not present");
         // =============================
 
-        // remove a genre from the book
+        // Update with only the database genre which should remove the programming genre & leave only database
         BookUpdateCommand bookUpdateCommand = new BookUpdateCommand();
         bookUpdateCommand.setId(fetchedBook.getId());
         bookUpdateCommand.setName("Java is great");
-        bookUpdateCommand.setGenres(Set.of(genreId));
+        bookUpdateCommand.setGenres(Set.of(database.getId()));
 
         request = HttpRequest.PUT("/books", bookUpdateCommand);
         httpClient.toBlocking().exchange(request, Argument.of(Book.class), Argument.of(JsonError.class));
@@ -104,29 +113,28 @@ public class BookControllerTest {
         // Fetch the updated book and check genres updates
         request = HttpRequest.GET("/books/" + bookId);
         fetchedBook = httpClient.toBlocking().retrieve(request, Book.class);
-        System.out.println(fetchedBook);
-
         assertEquals( totalGenres, genreRepository.count().block() );
         assertEquals( totalGenres - 2, fetchedBook.getGenres().size());
+        assertNotNull(findGenre(dbGenre, fetchedBook), "Database genre is not present");
 
         // Update book again with all 3 genres
         bookUpdateCommand.setName("Java is great");
-        bookUpdateCommand.setGenres(genres.stream().map(Genre::getId).collect(Collectors.toSet()));
+        bookUpdateCommand.setGenres(Set.of(programming, database, threads).stream().map(Genre::getId).collect(Collectors.toSet()));
 
         request = HttpRequest.PUT("/books", bookUpdateCommand);
         httpClient.toBlocking().exchange(request, Argument.of(Book.class), Argument.of(JsonError.class));
 
-        // Fetch the updated book and check genres updates
+        // Fetch the updated book and check all 3 genres are associated
         request = HttpRequest.GET("/books/" + bookId);
         fetchedBook = httpClient.toBlocking().retrieve(request, Book.class);
-        System.out.println(fetchedBook);
-
         assertEquals( totalGenres , genreRepository.count().block() );
         assertEquals( totalGenres, fetchedBook.getGenres().size());
+        assertNotNull(findGenre(progGenre, fetchedBook), "Programming genre is not present");
+        assertNotNull(findGenre(dbGenre, fetchedBook), "Database genre is not present");
+        assertNotNull(findGenre(thrGenre, fetchedBook), "Threads genre is not present");
 
         // Remove all genres associated with book
-        bookUpdateCommand.setGenres(Set.of());        // you can do this or just omit, same result, no genre list = remove all
-
+        bookUpdateCommand.setGenres(Set.of());
         request = HttpRequest.PUT("/books", bookUpdateCommand);
         httpClient.toBlocking().exchange(request, Argument.of(Book.class), Argument.of(JsonError.class));
 
@@ -137,9 +145,8 @@ public class BookControllerTest {
         assertEquals( totalGenres , genreRepository.count().block() );
         assertNull( fetchedBook.getGenres() );
 
-        // Use PATCH to update genres
+        // Use PATCH to update genres only
         bookUpdateCommand.setGenres(genres.stream().map(Genre::getId).collect(Collectors.toSet()));
-
         request = HttpRequest.PATCH("/books", bookUpdateCommand);
         httpClient.toBlocking().exchange(request, Argument.of(Book.class), Argument.of(JsonError.class));
 
@@ -153,6 +160,10 @@ public class BookControllerTest {
         // this is failing I think because @Transactional is needed on the BookRepository updateGenres method but then the update fails above with
         // 'No reactive transaction management has been configured. Ensure you have correctly configured a reactive capable transaction manager'
         //assertEquals( totalGenres, fetchedBook.getGenres().size());
+    }
+
+    private Genre findGenre(String genreName, Book book) {
+        return book.getGenres().stream().filter(g-> Objects.equals(g.getName(), genreName)).findAny().orElseGet(null);
     }
 
     protected UUID entityId(HttpResponse<?> response, String path) {
